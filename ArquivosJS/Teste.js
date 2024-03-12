47af388df7992fc0f71e93f05c6d45abf3286762
Hanjil <luiza.santosdiogo@gmail.com>
2024-03-12 20:11:43 -0300

    Botoes

--- ArquivosJS/Teste.js
+++ ArquivosJS/Teste.js
@@ -6,7 +6,3 @@ function toggleBotoesFundo() {
     var Fundos = document.getElementById("AtivaBotoesFundo");
     Fundos.style.display = (Fundos.style.display === "block") ? "none" : "block";
 }
-document.addEventListener("DOMContentLoaded", function () {
-    var Fundos = document.getElementById("AtivaBotoesFundo");
-    Fundos.style.display = "none";
-});
