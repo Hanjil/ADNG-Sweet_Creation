9dcdcda126305b8f400e4df9ce48118f9059ea44
Hanjil <luiza.santosdiogo@gmail.com>
2024-03-13 21:48:06 -0300

    Botão fundo - funcional e posicionado

--- ArquivosJS/Teste.js
+++
@@ -1,8 +0,0 @@
-function ProximoBG(fileName) {
-    var BGSelecionado = fileName;
-    document.getElementById('Fundo').src = fileName;
-}
-function toggleBotoesFundo() {
-    var Fundos = document.getElementById("AtivaBotoesFundo");
-    Fundos.style.display = (Fundos.style.display === "block") ? "none" : "block";
-}
