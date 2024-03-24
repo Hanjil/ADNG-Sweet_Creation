e1dcc5a85b14d2cdde6dd27d3990731c2cf9a268
Hanjil <luiza.santosdiogo@gmail.com>
2024-03-24 13:11:52 -0300

    TesteDownload

--- Canvas.js
+++ Canvas.js
@@ -22,49 +22,30 @@ function desenharCanvas() {
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const imgFundo = new Image();
-    imgFundo.src = FundoSelecionado;
-    imgFundo.onload = function() {
-        ctx.drawImage(imgFundo, 0, 0, canvas.width, canvas.height);
+    const carregarImagem = function(src) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = function() {
+                resolve(img);
+            };
+            img.onerror = reject;
+            img.src = src;
+        });
     };
 
-    const imgCabeloTras = new Image();
-    imgCabeloTras.src = CabeloTrasSelecionado;
-    imgCabeloTras.onload = function() {
-        ctx.drawImage(imgCabeloTras, 0, 0, canvas.width, canvas.height);
-    };
-
-    const imgPele = new Image();
-    imgPele.src = PeleSelecionado;
-    imgPele.onload = function() {
-        ctx.drawImage(imgPele, 0, 0, canvas.width, canvas.height);
-    };
-
-    const imgRoupa = new Image();
-    imgRoupa.src = RoupaSelecionada;
-    imgRoupa.onload = function() {
-        ctx.drawImage(imgRoupa, 0, 0, canvas.width, canvas.height);
-    };
-
-    const imgCabeloFrente = new Image();
-    imgCabeloFrente.src = CabeloFrenteSelecionado;
-    imgCabeloFrente.onload = function() {
-        ctx.drawImage(imgCabeloFrente, 0, 0, canvas.width, canvas.height);
-    };
-
-    const imgTaki = new Image();
-    imgTaki.src = TakiSelecionado;
-    imgTaki.onload = function() {
-        ctx.drawImage(imgTaki, 0, 0, canvas.width, canvas.height);
-    };
-
-    if (AccSelecionado !== 'Imagens/blank.png') {
-        const imgAcc = new Image();
-        imgAcc.src = AccSelecionado;
-        imgAcc.onload = function() {
-            ctx.drawImage(imgAcc, 0, 0, canvas.width, canvas.height);
-        };
-    }
+    Promise.all([
+        carregarImagem(FundoSelecionado),
+        carregarImagem(CabeloTrasSelecionado),
+        carregarImagem(PeleSelecionado),
+        carregarImagem(RoupaSelecionada),
+        carregarImagem(CabeloFrenteSelecionado),
+        carregarImagem(AccSelecionado),
+        carregarImagem(TakiSelecionado)
+    ]).then((imagens) => {
+        imagens.forEach((img) => {
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        });
+    });
 }
 
 function BaixarCanvas() {
